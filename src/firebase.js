dd899798c542d1b712be1afe908c701fddc55006
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-04 10:32:35 +0530

    Added firebaseConfig from 'Project Setting' -> 'Firebase SDK snippet' -> 'Config'

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDa6kCF8IbShM8_3idDpG9b5mc3Kb75hyQ",
+    authDomain: "slack-clone2-5fffd.firebaseapp.com",
+    projectId: "slack-clone2-5fffd",
+    storageBucket: "slack-clone2-5fffd.appspot.com",
+    messagingSenderId: "565082779298",
+    appId: "1:565082779298:web:594004a538b282341a31fb"
+  };
